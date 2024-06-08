e4532450b533ddef686215fa0df417c77526318c
Rye29 <167343946+Rye29@users.noreply.github.com>
2024-06-07 21:28:53 -0700

    Patch 1
    fixed bug where load.js loaded into end screen as opposed to the title screen

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -70,7 +70,7 @@ class Load extends Phaser.Scene {
         });
 
          // ...and pass to the next Scene
-         this.scene.start("Credit");
+         this.scene.start("Title");
     }
 
     // Never get here since a new scene is started in create()
